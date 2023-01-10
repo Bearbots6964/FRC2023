3a72ef90710edd0aa13fa06fad53637bd3a5b437
=TheGamer1002 <=48131223+TheGamer1002@users.noreply.github.com>
2023-01-10 16:23:15 -0500

    add more stuff to shuffleboard

--- src/main/java/frc/robot/commands/VisionInterface.java
+++ src/main/java/frc/robot/commands/VisionInterface.java
@@ -1,6 +1,10 @@
 package frc.robot.commands;
 import java.util.List;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 
@@ -53,7 +57,15 @@ public class VisionInterface extends CommandBase {
         Transform3d pose = target.getBestCameraToTarget();
         List<TargetCorner> corners = target.getDetectedCorners();
 
-        // Now, we check if trackingType from the Constants file is set to "tape" or "tag".
+        // Send all this data to shuffleboard.
+        Shuffleboard.getTab("Vision").add("Yaw", yaw);
+        Shuffleboard.getTab("Vision").add("Pitch", pitch);
+        Shuffleboard.getTab("Vision").add("Area", area);
+        Shuffleboard.getTab("Vision").add("Pose", pose);
+        
+        Shuffleboard.getTab("Vision").add("Corners", corners);
+
+        // Now, we check if trackingType from the Constants file is set to "tape" or "tag". We then send the relevant data to Shuffleboard.
         var trackingType = Constants.VisionConstants.kTrackingType;
         if (trackingType == tape) {
             // If it is set to "tape", get the skew of the target.
@@ -64,29 +76,12 @@ public class VisionInterface extends CommandBase {
             double poseAmbiguity = target.getPoseAmbiguity();
             Transform3d bestCameraToTarget = target.getBestCameraToTarget();
             Transform3d alternateCameraToTarget = target.getAlternateCameraToTarget();
+            Shuffleboard.getTab("Vision").addNumber("Target ID", targetIDSupplier);
+            Shuffleboard.getTab("Vision").addNumber("Pose Ambiguity", poseAmbiguitySupplier);
+            Shuffleboard.getTab("Vision").add("Best Camera to Target", bestCameraToTarget);
+            Shuffleboard.getTab("Vision").add("Alternate Camera to Target", alternateCameraToTarget);
         }
-        
 
-        // Send all this data to shuffleboard.
-        Shuffleboard.getTab("Vision").add("Yaw", yaw);
-        Shuffleboard.getTab("Vision").add("Pitch", pitch);
-        Shuffleboard.getTab("Vision").add("Area", area);
-        Shuffleboard.getTab("Vision").add("Pose", pose);
-        
-        Shuffleboard.getTab("Vision").add("Corners", corners);
-
-        // Now, we check if trackingType from the Constants file is set to "tape" or "tag".
-        var trackingType = Constants.VisionConstants.kTrackingType;
-        if (trackingType == tape) {
-            // If it is set to "tape", get the skew of the target.
-            Shuffleboard.getTab("Vision").addNumber("Skew", skew);
-        } else if (trackingType == tag) {
-            // Get information from target.
-            Shuffleboard.getTab("Vision").addNumber("Target ID", targetID);
-            Shuffleboard.getTab("Vision").addNumber("Pose Ambiguity", poseAmbiguity);
-            Shuffleboard.getTab("Vision").addNumber("Best Camera to Target", bestCameraToTarget);
-            Shuffleboard.getTab("Vision").addNumber("Alternate Camera to Target", alternateCameraToTarget);
-        }
 
 
 
@@ -97,4 +92,90 @@ public class VisionInterface extends CommandBase {
      public boolean isFinished() {
          return false;
      }
+
+
+/*
+     // Functions to get relevant data types.
+     public PhotonPipelineResult result() {
+        return m_Vision.getLatestResult();
+     }
+    public boolean hasTargets() {
+        return result().hasTargets();
+    }
+    public List<PhotonTrackedTarget> targets() {
+        return result().getTargets();
+    }
+    public PhotonTrackedTarget target() {
+        return result().getBestTarget();
+    }
+    public DoubleSupplier yaw() {
+        return target().getYaw();
+    }
+    public DoubleSupplier pitch() {
+        return target().getPitch();
+    }
+    public DoubleSupplier area() {
+        return target().getArea();
+    }
+    public Transform3d pose() {
+        return target().getBestCameraToTarget();
+    }
+    public double skew() {
+        if (Constants.VisionConstants.kTrackingType == tape) {
+            return target().getSkew();
+        } else {
+            throw(new IllegalArgumentException("Tracking type is not set to tape."));
+        }
+    }
+    public int targetID() {
+        if (Constants.VisionConstants.kTrackingType == tag) {
+            return target().getFiducialId();
+        } else {
+            throw(new IllegalArgumentException("Tracking type is not set to tag."));
+        }
+    }
+    public double poseAmbiguity() {
+        if (Constants.VisionConstants.kTrackingType == tag) {
+            return target().getPoseAmbiguity();
+        } else {
+            throw(new IllegalArgumentException("Tracking type is not set to tag."));
+        }
+    }
+    public Transform3d bestCameraToTarget() {
+        if (Constants.VisionConstants.kTrackingType == tag) {
+            return target().getBestCameraToTarget();
+        } else {
+            throw(new IllegalArgumentException("Tracking type is not set to tag."));
+        }
+    }
+    public Transform3d alternateCameraToTarget() {
+        if (Constants.VisionConstants.kTrackingType == tag) {
+            return target().getAlternateCameraToTarget();
+        } else {
+            throw(new IllegalArgumentException("Tracking type is not set to tag."));
+        }
+    }
+    */
+
+
+    public PhotonPipelineResult result() {
+        return m_Vision.getLatestResult();
+     }
+    public PhotonTrackedTarget target() {
+        return result().getBestTarget();
+    }
+
+    public BooleanSupplier hasTargetsSupplier = () -> result().hasTargets();
+
+    public DoubleSupplier yawSupplier = () -> target().getYaw();
+    public DoubleSupplier pitchSupplier = () -> target().getPitch();
+    public DoubleSupplier areaSupplier = () -> target().getArea();
+    public Supplier<Transform3d> poseSupplier = () -> target().getBestCameraToTarget();
+    public DoubleSupplier skewSupplier = () -> target().getSkew();
+    public DoubleSupplier targetIDSupplier = () -> target().getFiducialId();
+    public DoubleSupplier poseAmbiguitySupplier = () -> target().getPoseAmbiguity();
+
+
+
+
 }
