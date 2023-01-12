8a4a821b0c453399fb42b64ce23c2995a2818ea3
TheGamer1002 <=48131223+TheGamer1002@users.noreply.github.com>
2023-01-12 09:41:00 -0500

    Implement a more covenient system for triggers

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -18,11 +19,19 @@ public class Vision extends SubsystemBase {
 
   final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
+  // Pipeline mode.
+  public static final String kLimelightPipelineKey = "limelight-pipeline";
+
   public Vision() {
     // Stuff goes here
     limelight = new PhotonCamera("limelight");
-  }
 
+    // Set default pipeline
+    if (!Preferences.containsKey(kLimelightPipelineKey)) {
+      Preferences.setInt(kLimelightPipelineKey, 1); // Default to tape
+    }
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
@@ -34,7 +43,22 @@ public class Vision extends SubsystemBase {
     // This method will be called once per scheduler run during simulation
   }
 
+  /**
+   * Get the latest pipeline result.
+   * @return The latest pipeline result.
+   */
   public PhotonPipelineResult getLatestResult() {
     return limelight.getLatestResult();
   }
+
+  /**
+   * Change the Limelight pipeline.
+   * @param pipeline The pipeline to change to.
+   */
+  public void changePipeline(int pipeline) {
+    limelight.setPipelineIndex(pipeline); // As of now, 1 is tape, 2 is tag
+    Preferences.setInt(kLimelightPipelineKey, pipeline); // Save the pipeline index to preferences so we know what we're doing
+  }
+
+  
 }
