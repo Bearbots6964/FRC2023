df1a1de67d52e16e791aa0edc612571010110bdd
TheGamer1002 <=48131223+TheGamer1002@users.noreply.github.com>
2023-01-13 09:09:56 -0500

    (JAVA-D1001) Undocumented method found Fixes #17

--- src/main/java/frc/robot/commands/VisionInterface.java
+++ src/main/java/frc/robot/commands/VisionInterface.java
@@ -122,6 +122,10 @@ public class VisionInterface extends CommandBase {
   }
   */
 
+  /**
+   * Returns the latest result from the PhotonVision pipeline.
+   * @return The latest result from the PhotonVision pipeline.
+   */
   public PhotonPipelineResult result() {
     return m_Vision.getLatestResult();
   }
