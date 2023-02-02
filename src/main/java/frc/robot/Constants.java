9eef0322bd638479a5d1a70c93e2205127be0b06
gaming <48131223+TheGamer1002@users.noreply.github.com>
2023-02-02 07:39:31 -0500

    fix some property errors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,40 +30,42 @@ public final class Constants {
     private static TrackingType tape;
     public static final TrackingType kTrackingType = tape;
   }
+  public class AutoConstants{
+    public final XboxController m_driverController = new XboxController(1);
+    public final Joystick m_driverJoystick = new Joystick(2);
+
+    
+
+
+    //BEN DRIVE CONSTANTS
+    public static final double ksVolts=5;
+    public static final double kvVoltSecondsPerMeter=5;
+    public static final double kaVoltSecondsSquaredPerMeter=5;
+
+    public static final double kPDriveVel=5;
+
+    public static final double kTrackwidthMeters = 0.55;
+    public final DifferentialDriveKinematics kDriveKinematics =
+    new DifferentialDriveKinematics(kTrackwidthMeters);
+
+    public static final double kMaxSpeedMetersPerSecond=5 ;
+    public static final double kMaxAccelerationMetersPerSecondSquared=5;
+
+    // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+    public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
+
+    public final int[] kLeftEncoderPorts= {0,1};
+    public final int[] kRightEncoderPorts= {2,3};
+    public static final boolean kLeftEncoderReversed = false;
+    public static final boolean kRightEncoderReversed = false;
+
+    public static final double kEncoderDistancePerPulse=5;
+    
+}
 }
 
     // Constants copied and pasted from 2020 code
-    public static final class AutoConstants{
-        public static final XboxController m_driverController = new XboxController(1);
-        public static final Joystick m_driverJoystick = new Joystick(2);
-
-        
-
-
-        //BEN DRIVE CONSTANTS
-        public static final double ksVolts=5;
-        public static final double kvVoltSecondsPerMeter=5;
-        public static final double kaVoltSecondsSquaredPerMeter=5;
     
-        public static final double kPDriveVel=5;
+    
 
-        public static final double kTrackwidthMeters = 0.55;
-        public static final DifferentialDriveKinematics kDriveKinematics =
-        new DifferentialDriveKinematics(kTrackwidthMeters);
-
-        public static final double kMaxSpeedMetersPerSecond=5 ;
-        public static final double kMaxAccelerationMetersPerSecondSquared=5;
-
-        // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
-        public static final double kRamseteB = 2;
-        public static final double kRamseteZeta = 0.7;
-
-        public static final int[] kLeftEncoderPorts= {0,1};
-        public static final int[] kRightEncoderPorts= {2,3};
-        public static final boolean kLeftEncoderReversed = false;
-        public static final boolean kRightEncoderReversed = false;
-
-        public static final double kEncoderDistancePerPulse=5;
-        
-    }
-}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -151,7 +151,7 @@ public class RobotContainer {
     //     .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Not sure if it works because I don't know how to use simulations
-    new JoystickButton(m_driverJoystick, 1).whileTrue(m_servoSubsystem.servoTest());
+    // new JoystickButton(m_driverJoystick, 1).whileTrue(m_servoSubsystem.servoTest());
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
