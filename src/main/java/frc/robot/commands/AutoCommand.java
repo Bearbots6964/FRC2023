770a78736d911ad59bf99c609be751bc8640db24
gaming <48131223+TheGamer1002@users.noreply.github.com>
2023-02-17 16:58:30 -0500

    add autocommand types, update modules

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -6,8 +6,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Claw;
 import frc.robot.subsystems.PID;
 import frc.robot.subsystems.Tank;
+import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.Vision;
 
 public class AutoCommand extends CommandBase {
   /** Example static factory for an autonomous command. */
@@ -15,12 +19,19 @@ public class AutoCommand extends CommandBase {
 
   private final PID pid;
   private double speed;
+  private final Turret turret;
+  private final Arm arm;
+  private final Claw claw;
+  private final Vision vision;
   private Timer timer = new Timer();
 
-  public AutoCommand(Tank drive1, PID m_pid, double m_speed) {
+  public AutoCommand(Tank drive1, PID m_pid, Turret m_turret, Arm m_arm, Claw m_claw, Vision m_vision) {
     drive = drive1;
     pid = m_pid;
-    speed = m_speed;
+    turret = m_turret;
+    arm = m_arm;
+    claw = m_claw;
+    vision = m_vision;
   }
 
   @Override
