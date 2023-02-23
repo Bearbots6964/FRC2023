b1fc6b6abf8358495c03d88805d2228f11d922a0
gaming <48131223+TheGamer1002@users.noreply.github.com>
2023-02-23 18:19:28 -0500

    Port frameworks to official Limelight libs

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -4,24 +4,46 @@
 
 package frc.robot.subsystems;
 
+import java.util.Map;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.*;
 
 public class Claw extends SubsystemBase {
   private CANSparkMax clawMotor;
+  ShuffleboardTab tab = Shuffleboard.getTab("Claw");
+    Map<String, Object> widgetProperties = Map.of("min", 0, "max", 20, "blockIncrement", 1);
+  private GenericEntry stallWidget = tab.add("Stall Limit", 10).withWidget(BuiltInWidgets.kNumberSlider).withProperties(widgetProperties).withPosition(0, 0).withSize(2, 1).getEntry();
+  private GenericEntry freeWidget = tab.add("Free Limit", 11).withWidget(BuiltInWidgets.kNumberSlider).withProperties(widgetProperties).withPosition(2, 0).withSize(2, 1).getEntry();
 
+
+  
+  
   // gear ratio is 100:1
 
   public Claw() {
     clawMotor = new CANSparkMax(8, MotorType.kBrushless);
     clawMotor.setIdleMode(IdleMode.kBrake);
-    clawMotor.setSmartCurrentLimit(6, 7);
+    clawMotor.setSmartCurrentLimit(10, 11);
     clawMotor.burnFlash();
+
+    // create widget for setting the claw smart current limit. one slider for stall limit, one for free limit
+    
+    
   }
 
-  public void periodic() {}
+  public void periodic() {
+    // set the smart current limit based on the values set in the shuffleboard widget
+    ShuffleboardTab tab = Shuffleboard.getTab("Claw");
+    int stallLimit = (int) stallWidget.getInteger(10);
+    int freeLimit = (int) freeWidget.getInteger(11);
+    clawMotor.setSmartCurrentLimit(stallLimit, freeLimit);
+  }
 
   public void closeClaw() {
     clawMotor.set(0.75);
