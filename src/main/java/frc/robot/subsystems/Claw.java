468a5d421d773e75ea6b3e6d7d1bc3780541c7df
gaming <48131223+TheGamer1002@users.noreply.github.com>
2023-02-25 13:24:41 -0500

    fix

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -10,11 +10,20 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.*;
 
 public class Claw extends SubsystemBase {
   private CANSparkMax clawMotor;
+  ShuffleboardTab tab = Shuffleboard.getTab("Claw");
+    Map<String, Object> widgetProperties = Map.of("min", 0, "max", 20, "blockIncrement", 1);
+  private GenericEntry stallWidget = tab.add("Stall Limit", 10).withWidget(BuiltInWidgets.kNumberSlider).withProperties(widgetProperties).withPosition(0, 0).withSize(2, 1).getEntry();
+  private GenericEntry freeWidget = tab.add("Free Limit", 11).withWidget(BuiltInWidgets.kNumberSlider).withProperties(widgetProperties).withPosition(2, 0).withSize(2, 1).getEntry();
 
+
+  
+  
   // gear ratio is 100:1
 
   public Claw() {
@@ -24,18 +33,15 @@ public class Claw extends SubsystemBase {
     clawMotor.burnFlash();
 
     // create widget for setting the claw smart current limit. one slider for stall limit, one for free limit
-    ShuffleboardTab tab = Shuffleboard.getTab("Claw");
-    Map<String, Object> widgetProperties = Map.of("min", 0, "max", 100, "blockIncrement", 1);
-    tab.add("Stall Limit", 10).withWidget("Number Slider").withProperties(widgetProperties).withPosition(0, 0).withSize(2, 1);
-    tab.add("Free Limit", 11).withWidget("Number Slider").withProperties(widgetProperties).withPosition(2, 0).withSize(2, 1);
-
+    
+    
   }
 
   public void periodic() {
     // set the smart current limit based on the values set in the shuffleboard widget
     ShuffleboardTab tab = Shuffleboard.getTab("Claw");
-    int stallLimit = (int) tab.getEntry("Stall Limit").getDouble(10);
-    int freeLimit = (int) tab.getEntry("Free Limit").getDouble(11);
+    int stallLimit = (int) stallWidget.getInteger(10);
+    int freeLimit = (int) freeWidget.getInteger(11);
     clawMotor.setSmartCurrentLimit(stallLimit, freeLimit);
   }
 
