e11742039e4bab969ca7467b54d009f2859068c6
gaming <48131223+TheGamer1002@users.noreply.github.com>
2023-02-25 13:04:12 -0500

    fix pid values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,7 +41,7 @@ public final class Constants {
 
   public static final class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    public static final double ProportionalDivisor = 16;
+    public static final double ProportionalDivisor = 18;
   }
 
   public static final class VisionConstants {
