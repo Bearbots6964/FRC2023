afc071f99201dfb467f2393eb8f520fcefe07186
ilian <ilianishere@gmail.com>
2023-03-23 14:36:56 -0400

    wip

---
+++ src/main/java/frc/robot/commands/InvertDriveCommand.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Tank;
+
+public class InvertDriveCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Tank m_drivebase;
+  private final RobotContainer robot;
+
+  public InvertDriveCommand(Tank subsystem, RobotContainer robot) {
+    m_drivebase = subsystem;
+    this.robot = robot;
+
+    addRequirements(m_drivebase);
+  }
+
+  @Override
+  public void initialize() {
+    RobotContainer.inverted = true;
+    robot.initTeleop();
+  }
+
+  @Override
+  public void execute() {
+    // double check getMaxSpeed(), might be wrong
+    m_drivebase.arcadeDrive(
+        -RobotContainer.getDriverControllerLeftStickYAdjusted() * Constants.CanConstants.maxSpeed,
+        -RobotContainer.getDriverControllerRightStickXAdjusted() * Constants.CanConstants.maxSpeed);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_drivebase.arcadeDrive(0, 0);
+    RobotContainer.inverted = false;
+    robot.initTeleop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public boolean runsWhenDisabled() {
+    return false;
+  }
+}
