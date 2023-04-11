df9695728aab7d1f0d915ffbeb3a4e330c75dacc
Computer 0 <48131223+TheGamer1002@users.noreply.github.com>
2023-04-11 09:11:36 -0400

    stuff

--- src/main/java/edu/bearbots/BearLib/drivebase/TankDrivebase.java
+++ src/main/java/edu/bearbots/BearLib/drivebase/TankDrivebase.java
@@ -1,4 +1,14 @@
 package edu.bearbots.BearLib.drivebase;
 
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 public abstract class TankDrivebase extends Drivebase {
+    private static DifferentialDrive differentialDrive;
+    public TankDrivebase(CANSparkMax frontLeft, CANSparkMax backLeft, CANSparkMax frontRight, CANSparkMax backRight) {
+        backLeft.follow(frontLeft);
+        backRight.follow(frontRight);
+        differentialDrive = new DifferentialDrive(frontLeft, frontRight);
+    }
 }
