050cc7f906ecf71c6074fc0f4eab5ddc382442d2
gaming <48131223+TheGamer1002@users.noreply.github.com>
2023-04-26 13:51:25 -0400

    Update RebindHat.java

--- src/main/java/frc/robot/RebindHat.java
+++ src/main/java/frc/robot/RebindHat.java
@@ -5,7 +5,17 @@ package frc.robot;
 @Deprecated
 public class RebindHat {
   public static double JoystickToYAxis() {
-
+    if (RobotContainer.m_armController.getPOV() == -1
+         || RobotContainer.m_armController.getPOV() == 90
+         || RobotContainer.m_armController.getPOV() == 270) {
+       return 0;
+     }
+     if (RobotContainer.m_armController.getPOV() > 270
+         || RobotContainer.m_armController.getPOV() < 90) {
+       return 1;
+     } else {
+       return -1;
+     }
   }
 
   public static double JoystickToXAxis() {
